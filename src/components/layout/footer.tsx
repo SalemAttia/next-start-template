293444097443ca4dfc681da333741236d23f8c5a
SalemAttia <salem.attia@jumia.com>
2020-10-20 10:26:10 +0200

    Build page structure

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="bg-accent-1 border-t border-accent-2">
+      Footer
+    </footer>
+  )
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_PATH = 'EXAMPLE.COM'
+export const SITE_NAME = 'NAME'
+export const HOME_IMAGE_URL = 'image'
+export const NAME = 'NAME'
