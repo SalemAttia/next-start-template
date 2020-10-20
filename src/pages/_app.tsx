54d1f77fe08afeb42201a5a9449b90c99c6a7cab
SalemAttia <salem.attia@jumia.com>
2020-10-21 00:57:17 +0200

    configure sentry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,15 @@ import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import { SWRConfig } from 'swr';
+import * as Sentry from '@sentry/node';
+
+
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+    Sentry.init({
+      enabled: process.env.NODE_ENV === 'production',
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN
+    });
+  }
 
 export default class MyApp extends App {
   componentDidMount() {
